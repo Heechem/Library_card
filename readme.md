c6d775ff208264a0801c2b28c9c05eaaba30f82b
Heechem <hichempeter@gmail.com>
2023-02-28 19:18:49 -0700

    Second commit

--- readme.md
+++ readme.md
@@ -1,4 +1,13 @@
 first added the css and HTML
+First day 1hour doing the display and UI
+Second day 1H30m added the function and also the element creation
+
 Working on the script to add the value
+
 Constructor Added to creat the book information.
+
 Added the submit action to push the book info into the array
+
+Added the function to create the div and append it to the DOM
+
+Card Added and Half of the program completed
--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const pages = document.getElementById('pages');
 const readen = document.getElementById('readen');
 const addBtn = document.querySelector('.add');
 const formField = document.querySelector('form');
+const container = document.querySelector('.card_container');
+
+let readBook;
+
 let addedBook;
 let myLibrary = [];
 
@@ -13,9 +17,16 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = 'not read yet';
-  this.info = function () {
-    return `${this.title} by ${author}, ${pages}, ${read}`;
-  };
+}
+
+// get the read it or not information
+
+function checkRead() {
+  if (readen.checked) {
+    readBook = 'I have read it';
+  } else {
+    readBook = " I haven't read it ";
+  }
 }
 
 const theHobbit = new Book(
@@ -24,18 +35,47 @@ const theHobbit = new Book(
   '298 pages',
   'not read yet '
 );
-
-console.log(theHobbit.info());
-
+// create tthe book from the consturctor
 function addBookToLibrary() {
   return (addedBook = new Book(author.value, title.value, pages.value));
 }
 
+// display the book
+function bookDisplay(book) {
+  book.map((x) => {
+    const author = x.author;
+    const title = x.title;
+    const pages = x.pages;
+    console.log(author, title, pages);
+    createCard(author, title, pages);
+  });
+}
+
+//append the card to the DOM
+
+function createCard(title, author, pages, readen) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.innerHTML = `
+  <button><i class="fas fa-trash"></i></button>
+  <h3>${title}</h3>
+  <p>This book was written by ${author} and contains ${pages} pages</p>
+  <span>${readBook}</span>
+  `;
+  container.appendChild(card);
+}
+
+//button listener
+
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
+  if (author.value == 0 || title.value == 0 || pages.value == 0) return;
   addBookToLibrary();
+  checkRead();
   myLibrary.push(addedBook);
   console.log(addedBook);
   console.log(myLibrary);
+  bookDisplay(myLibrary);
   formField.reset();
+  myLibrary.shift();
 });
